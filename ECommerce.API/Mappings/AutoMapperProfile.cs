02d4827c4cc4d499fcdcf20b30cc8e1c9337bb4a
Naman Padaliya <naman@mutualart.com>
2024-12-27 12:03:46 +0530

    feat(api): add AutoMapperProfile for object mapping
    - Introduced AutoMapperProfile to configure and manage object mappings

---
+++ ECommerce.API/Mappings/AutoMapperProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using ECommerce.Application.DTOs;
+using ECommerce.Domain.Entities;
+
+namespace ECommerce.API.Mappings
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<Product, ProductCreateDTO>().ReverseMap();
+            CreateMap<Product, ProductUpdateDTO>().ReverseMap();
+
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Category, CategoryCreateDTO>().ReverseMap();
+            CreateMap<Category, CategoryUpdateDTO>().ReverseMap();
+
+            CreateMap<Order, OrderDTO>().ReverseMap();
+            CreateMap<Order, OrderCreateDTO>().ReverseMap();
+            CreateMap<Order, OrderUpdateDTO>().ReverseMap();
+
+            CreateMap<OrderItem, OrderItemDTO>().ReverseMap();
+            CreateMap<OrderItem, OrderItemCreateDTO>().ReverseMap();
+            CreateMap<OrderItem, OrderItemUpdateDTO>().ReverseMap();
+        }
+    }
+}
