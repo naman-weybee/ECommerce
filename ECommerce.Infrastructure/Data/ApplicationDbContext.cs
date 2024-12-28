ca57bfacb6f5a1dd3148ec59f963a3c1de9c1f60
Naman Padaliya <naman@mutualart.com>
2024-12-28 19:02:43 +0530

    chore(infrastructure): adjust namespace in DbContext

--- ECommerce.Infrastructure/Data/ApplicationDbContext.cs
+++ ECommerce.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
 ﻿using ECommerce.Domain.Entities;
-using ECommerce.Domain.ValueConverters;
+using ECommerce.Domain.ValueObjects.ValueConverters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Linq.Expressions;
