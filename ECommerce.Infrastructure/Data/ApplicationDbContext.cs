fae5a21a277a477d6d6aad6dad01c5e0a32ca37b
Naman Padaliya <naman@mutualart.com>
2024-12-30 12:09:08 +0530

    refactor(api): update DateTime.Now to DateTime.UtcNow in ApplicationDbContext SaveChanges method

--- ECommerce.Infrastructure/Data/ApplicationDbContext.cs
+++ ECommerce.Infrastructure/Data/ApplicationDbContext.cs
@@ -33,7 +33,7 @@ namespace ECommerce.Infrastructure.Data
                 {
                     item.State = EntityState.Unchanged;
                     baseDto.IsDeleted = true;
-                    baseDto.DeletedDate = DateTime.Now;
+                    baseDto.DeletedDate = DateTime.UtcNow;
                 }
             }
 
