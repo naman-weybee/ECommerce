81b668f03016037d09ff79e9d0c22a1db4d3d45c
Naman Padaliya <naman@mutualart.com>
2024-12-30 18:24:04 +0530

    chore(api): refactor service registrations and clean Program.cs
    - Moved service registrations to ServiceCollectionExtensions:
      - AddAPIServices
      - AddApplicationServices
      - AddDomainServices
      - AddInfrastructureServices
    
    - Cleaned up Program.cs for better maintainability

--- ECommerce.API/Extensions/ServiceCollectionExtensions.cs
+++ ECommerce.API/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,59 @@
-﻿namespace ECommerce.API.Extensions
+﻿using ECommerce.API.Mappings;
+using ECommerce.Application.Interfaces;
+using ECommerce.Application.Services;
+using ECommerce.Domain.Aggregates;
+using ECommerce.Domain.Entities;
+using ECommerce.Domain.ValueObjects.JsonConverters;
+using ECommerce.Infrastructure.Data;
+using ECommerce.Infrastructure.Repositories;
+using ECommerce.Shared.Interfaces;
+using ECommerce.Shared.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.API.Extensions
 {
-    public class ServiceCollectionExtensions
+    public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddAPIServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IOrderService, OrderService>();
+
+            services.AddScoped<IPaginationService, PaginationService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddDomainServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository<ProductAggregate, Product>, Repository<ProductAggregate, Product>>();
+            services.AddScoped<IRepository<CategoryAggregate, Category>, Repository<CategoryAggregate, Category>>();
+            services.AddScoped<IRepository<OrderAggregate, Order>, Repository<OrderAggregate, Order>>();
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.Converters.Add(new MoneyJsonConverter());
+                options.SerializerSettings.Converters.Add(new CurrencyJsonConverter());
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+            return services;
+        }
     }
 }
--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,14 +1,4 @@
-using ECommerce.API.Mappings;
-using ECommerce.Application.Interfaces;
-using ECommerce.Application.Services;
-using ECommerce.Domain.Aggregates;
-using ECommerce.Domain.Entities;
-using ECommerce.Domain.ValueObjects.JsonConverters;
-using ECommerce.Infrastructure.Data;
-using ECommerce.Infrastructure.Repositories;
-using ECommerce.Shared.Interfaces;
-using ECommerce.Shared.Repositories;
-using Microsoft.EntityFrameworkCore;
+using ECommerce.API.Extensions;
 
 namespace ECommerce.API
 {
@@ -18,27 +8,11 @@ namespace ECommerce.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-
-            builder.Services.AddControllers().AddNewtonsoftJson(options =>
-            {
-                options.SerializerSettings.Converters.Add(new MoneyJsonConverter());
-                options.SerializerSettings.Converters.Add(new CurrencyJsonConverter());
-            });
-
-            builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
-
-            builder.Services.AddScoped<IProductService, ProductService>();
-            builder.Services.AddScoped<ICategoryService, CategoryService>();
-            builder.Services.AddScoped<IOrderService, OrderService>();
-
-            builder.Services.AddScoped<IPaginationService, PaginationService>();
-
-            builder.Services.AddScoped<IRepository<ProductAggregate, Product>, Repository<ProductAggregate, Product>>();
-            builder.Services.AddScoped<IRepository<CategoryAggregate, Category>, Repository<CategoryAggregate, Category>>();
-            builder.Services.AddScoped<IRepository<OrderAggregate, Order>, Repository<OrderAggregate, Order>>();
+            builder.Services
+                .AddAPIServices()
+                .AddApplicationServices()
+                .AddDomainServices()
+                .AddInfrastructureServices(builder.Configuration);
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
